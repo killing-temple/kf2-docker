381ba10773255a4174d06e6a959afd52588a4552
lygais <lygais@killingtemple.com>
2020-11-19 20:59:05 -0500

    set start.sh to use sh instead of bash

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 tmux new -s kf2server -d
 tmux send-keys -t "kf2server" '/home/steam/.steam/SteamApps/common/kf2server/Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-the-killing-temple' Enter
