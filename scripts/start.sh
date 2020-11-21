c6be98473a76029466c7f1654339cf9f8facc52d
lygais <lygais@killingtemple.com>
2020-11-21 14:56:26 -0500

    start script tweaks

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 # tmux new -s kf2server -d
 # tmux send-keys -t "kf2server" '/home/steam/kf2server/Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-the-killing-temple' Enter
 ls -la kf2server
-/home/steam/kf2server/Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-the-killing-temple
+/home/steam/kf2server/Binaries/Win64/KFGameSteamServer.bin.x86_64
