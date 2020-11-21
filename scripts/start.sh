3e229c8f8fe774ffa9b41b43db80043925f2d243
lygais <lygais@killingtemple.com>
2020-11-21 17:18:56 -0500

    polishing start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "STARTING $KF2SERVER"
-mount --bind /home/steam/kf2-game-configs/$KF2SERVER /home/steam/kf2server/KFGame/Config
+ln -s /home/steam/kf2-game-configs/$KF2SERVER /home/steam/kf2server/KFGame/Config
 /home/steam/kf2server/Binaries/Win64/KFGameSteamServer.bin.x86_64
