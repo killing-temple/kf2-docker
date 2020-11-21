fffe9af0372a2841f49bf238df07d10eec4377dd
lygais <lygais@killingtemple.com>
2020-11-21 16:31:44 -0500

    ok lets just use ubuntu:focal

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# tmux new -s kf2server -d
-# tmux send-keys -t "kf2server" '/home/steam/kf2server/Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-the-killing-temple' Enter
-cd kf2server/Binaries/Win64
-./KFGameSteamServer.bin.x86_64
+echo "STARTING $KF2SERVER"
+tmux new -s kf2server -d
+tmux send-keys -t "kf2server" '/home/steam/kf2server/Binaries/Win64/KFGameSteamServer.bin.x86_64' Enter
+# cd kf2server/Binaries/Win64
+# ./KFGameSteamServer.bin.x86_64
