f8f897384e454a5557950cf912a712e972ab97b0
lygais <lygais@killingtemple.com>
2020-11-24 01:35:49 -0500

    added sudo for mount binding

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "STARTING $KF2SERVER"
 pkill ping
-ln -s /home/steam/kf2-game-configs/$KF2SERVER /home/steam/kf2server/KFGame/Config
+# ln -s /home/steam/kf2-game-configs/$KF2SERVER /home/steam/kf2server/KFGame/Config
+sudo mount --bind /home/steam/kf2-game-configs/$KF2SERVER /home/steam/kf2server/KFGame/Config
 /home/steam/kf2server/Binaries/Win64/KFGameSteamServer.bin.x86_64
