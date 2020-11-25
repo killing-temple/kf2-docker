62bed18fe0d4b7f3cfeb70b7d31857c278258140
lygais <lygais@killingtemple.com>
2020-11-25 18:57:23 -0500

    trying configsubdir

--- CREATE.sh
+++ CREATE.sh
@@ -8,10 +8,16 @@
 # stable: /home/steam/.steam/SteamApps/common/kf2server
 # beta:   /home/steam/.steam/SteamApps/beta/kf2server
 
+cd /tmp;
+git clone https://github.com/killing-temple/kf2-game-configs.git;
+sudo cp ./kf2-game-configs/* /home/steam/.steam/SteamApps/common/kf2server/KFGame/Config/ -R
+sudo cp ./kf2-game-configs/* /home/steam/.steam/SteamApps/beta/kf2server/KFGame/Config/ -R
+
 sudo docker rm --force main
 sudo docker run -d --name=main \
     -v "/home/steam/.steam/SteamApps/common/kf2server:/kf2server" \
     -e KF2SERVER=main \
+    -e WEBPORT=8080 \
     -p 0.0.0.0:8080:8080/tcp \
     -p 0.0.0.0:7777:7777/udp \
     -p 0.0.0.0:27015:27015/udp \
@@ -22,6 +28,7 @@ sudo docker rm --force main2
 sudo docker run -d --name=main2 \
     -v "/home/steam/.steam/SteamApps/common/kf2server:/kf2server" \
     -e KF2SERVER=main \
+    -e WEBPORT=8081 \
     -p 0.0.0.0:8081:8080/tcp \
     -p 0.0.0.0:7778:7777/udp \
     -p 0.0.0.0:27016:27015/udp \
@@ -32,6 +39,7 @@ sudo docker rm --force main3
 sudo docker run -d --name=main3 \
     -v "/home/steam/.steam/SteamApps/common/kf2server:/kf2server" \
     -e KF2SERVER=main \
+    -e WEBPORT=8082 \
     -p 0.0.0.0:8082:8080/tcp \
     -p 0.0.0.0:7779:7777/udp \
     -p 0.0.0.0:27017:27015/udp \
@@ -42,6 +50,7 @@ sudo docker rm --force mainbeta
 sudo docker run -d --name=mainbeta \
     -v "/home/steam/.steam/SteamApps/beta/kf2server:/kf2server" \
     -e KF2SERVER=main-beta \
+    -e WEBPORT=8083 \
     -p 0.0.0.0:8083:8080/tcp \
     -p 0.0.0.0:7780:7777/udp \
     -p 0.0.0.0:27018:27015/udp \
@@ -52,6 +61,7 @@ sudo docker rm --force mainbeta2
 sudo docker run -d --name=mainbeta2 \
     -v "/home/steam/.steam/SteamApps/beta/kf2server:/kf2server" \
     -e KF2SERVER=main-beta \
+    -e WEBPORT=8084 \
     -p 0.0.0.0:8084:8080/tcp \
     -p 0.0.0.0:7781:7777/udp \
     -p 0.0.0.0:27019:27015/udp \
@@ -62,6 +72,7 @@ sudo docker rm --force mainbeta3
 sudo docker run -d --name=mainbeta3 \
     -v "/home/steam/.steam/SteamApps/beta/kf2server:/kf2server" \
     -e KF2SERVER=main-beta \
+    -e WEBPORT=8085 \
     -p 0.0.0.0:8085:8080/tcp \
     -p 0.0.0.0:7782:7777/udp \
     -p 0.0.0.0:27020:27015/udp \
